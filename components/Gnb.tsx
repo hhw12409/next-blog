ed69a65bf19a4509172decb823486cbf6d5b604f
hhw12409 <sksk2425@naver.com>
2022-12-23 02:52:59 +0900

    feat: add Banner profile

--- components/Gnb.tsx
+++ components/Gnb.tsx
@@ -1,24 +1,41 @@
+import Image from "next/image";
+
 export default function Gnb() {
   return (
     <nav>
-      <h1>Frontend Developer Blog</h1>
-      <ul>
-        <li>Home</li>
-        <li>Home</li>
-      </ul>
+      <div>
+        <Image
+          src={"/images/profile-image.png"}
+          width={120}
+          height={120}
+          style={{ borderRadius: 60 }}
+          alt="profile-image"
+        />
+        <h1>Frontend Developer Blog</h1>
+        <p>Nice to Meet You,</p>
+        <p>I&apos;m Junior Frontend Developer Hyeonwoo.</p>
+      </div>
       <style jsx>{`
         nav {
           display: flex;
+          flex-direction: column;
           background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
           color: #ffffff;
-          justify-content: space-between;
-          align-items: center;
-          height: 12rem;
+          align-items: flex-start;
+          justify-content: center;
           width: 100%;
           position: fixed;
+          height: 25rem;
         }
-        nav > ul {
-          display: flex;
+        div {
+          width: 768px;
+          margin: 0 auto;
+        }
+        @media (max-width: 768px) {
+          nav {
+            height: 300px;
+            padding: 0 20px;
+          }
         }
       `}</style>
     </nav>
