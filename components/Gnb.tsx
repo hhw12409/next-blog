dbbbccb7c358e1ae5ddae35a6ee866397432caec
hhw12409 <sksk2425@naver.com>
2022-12-23 02:13:58 +0900

    style: delete scroll 감지 기능

--- components/Gnb.tsx
+++ components/Gnb.tsx
@@ -1,25 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
+import COLORS from "../constants/colors";
 
 export default function Gnb() {
-  const [scroll, setScroll] = useState(false);
-
-  const handleScroll = useCallback(() => {
-    // 스크롤이 Top에서 50px 이상 내려오면 true값을 useState에 넣어줌
-    if (window.scrollY >= 50) {
-      setScroll(true);
-    } else {
-      // 스크롤이 50px 미만일경우 false를 넣어줌
-      setScroll(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll); //clean up
-    };
-  }, [handleScroll]);
-
   return (
     <nav>
       <h1>Frontend Developer Blog</h1>
@@ -30,13 +12,13 @@ export default function Gnb() {
       <style jsx>{`
         nav {
           display: flex;
-          background-color: red;
+          background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+          color: #ffffff;
           justify-content: space-between;
           align-items: center;
-          height: 3rem;
+          height: 12rem;
           width: 100%;
           position: fixed;
-          box-shadow: ${scroll ? "0 0 16px rgb(0 0 0 / 28%)" : null};
         }
         nav > ul {
           display: flex;
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
