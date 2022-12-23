1419bc0a76df321a344ae1ac832dc9f4432c9934
hhw12409 <sksk2425@naver.com>
2022-12-23 22:31:12 +0900

    feat: Banner Profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -3,24 +3,41 @@ import Image from "next/image";
 export default function Profile() {
   return (
     <div>
-        <Image
-          src={"/images/profile-image.png"}
-          width={120}
-          height={120}
-          style={{ borderRadius: 60 }}
-          alt="profile-image"
-        />
-        <h1>Frontend Developer Blog</h1>
-        <p>Nice to Meet You,</p>
-        <p>I&apos;m Junior Frontend Developer Hyeonwoo.</p>
-        <style jsx>{`
-        
+      <Image
+        src={"/images/profile-image.png"}
+        width={120}
+        height={120}
+        style={{ borderRadius: 60 }}
+        alt="profile-image"
+      />
+      <h1>Nice to Meet You,</h1>
+      <p>I&apos;m Junior Frontend Developer Hyeonwoo.</p>
+      <style jsx>{`
         div {
           width: 768px;
           margin: 0 auto;
         }
-        
+        div > h1 {
+          margin-top: 25px;
+          font-size: 35px;
+          font-weight: 700;
+        }
+
+        div > p {
+          font-size: 20px;
+          font-weight: 400;
+        }
+
+        @media (max-width: 768px) {
+          div > h1 {
+            font-size: 25px;
+          }
+
+          div > p {
+            font-size: 15px;
+          }
+        }
       `}</style>
-      </div>
-  )
+    </div>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Source+Sans+Pro:wght@300;400&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
